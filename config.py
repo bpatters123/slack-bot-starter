8f3798b45d78a43c79707b99145d48930a90aa11
Brad Patterson <bpatters123@gmail.com>
2021-03-28 15:04:12 -0400

    added envirinment variables for slack tokens

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     # slack tokens
-    SLACK_VERIFICATION_TOKEN = ''
-    SLACK_OAUTH_TOKEN = ''
-    SLACK_BOT_TOKEN = ''
+    SLACK_VERIFICATION_TOKEN = os.environ.get('SLACK_VERIFICATION_TOKEN')
+    SLACK_OAUTH_TOKEN = os.environ.get('SLACK_OAUTH_TOKEN')
+    SLACK_BOT_TOKEN = os.environ.get('SLACK_BOT_TOKEN')
--- webapp/__init__.py
+++ webapp/__init__.py
@@ -8,7 +8,11 @@ app.config.from_object(Config)
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello World!"
+    return "Hello World! "+ app.config['SLACK_OAUTH_TOKEN']
+
+@app.route('/echo')
+def echo():
+    return jsonify(text="Hello")
 
 if __name__ == "__main__":
     app.run()
